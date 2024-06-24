f354561be1e5eb93e6035208410f309608e3696b
Sonicetorock <sonishgames@gmail.com>
2024-06-25 00:49:27 +0530

    FE :added components and updated the adminDashboard

--- client/src/pages/admin/AdminDashboardPage.jsx
+++ client/src/pages/admin/AdminDashboardPage.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 
+import CardBarChart from '../../components/CardBarChart'
+import CardLineChart from '../../components/CardLineChart'
+import CardPageVisits from '../../components/CardPageVisits'
+import CardSocialTraffic from '../../components/CardSocialTraffic'
 const AdminDashboardPage = () => {
   return (
-    <div>
+    <>
+    {/* <div>
       <h1>welcome to admindashboard</h1>
+    </div> */}
+    <div className="flex flex-wrap">
+      <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+        <CardLineChart />
+      </div>
+      <div className="w-full xl:w-4/12 px-4">
+        <CardBarChart />
+      </div>
     </div>
+    <div className="flex flex-wrap mt-4">
+      <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+        <CardPageVisits />
+      </div>
+      <div className="w-full xl:w-4/12 px-4">
+        <CardSocialTraffic />
+      </div>
+    </div>
+  </>
   )
 }
 
